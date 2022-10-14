f0291d3276912628cea7bf0a11e5e69a3a369072
RyanGarccia <41078173+RyanGarccia@users.noreply.github.com>
2022-10-14 18:19:45 -0300

    Update message.php

--- message.php
+++ message.php
@@ -6,6 +6,4 @@ $phone		= $_POST["phone"];
 
 $response = array("reply" => "Olá, $sender segue a nossa chave pix: 91993508026.");
 echo json_encode($response);
-
-$response = array("$message" = "pix, Pix, PIX" => "teste de soliciteção do servidor");
 ?>
